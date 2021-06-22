480ab0aa86c0766089a3fcbe67ca4be50e04107c
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2021-06-22 22:38:37 +0300

    First working version (#1)
    * Add textField component (not ready yet!)
    
    * Add lottery logic (made earlier to my separate Dart project)
    
    * Add a settings button into AppBar (not ready yet!)
    
    * Add 'circular_countdown_timer' library & use it in the main.dart (+ remove unnecessary old parts)
    
    * Finish the 1st working version
    
    * Update href in index.html

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:flutter_participants_lottery/lottery_logic.dart' as lottery;
 
 void main() {
   runApp(MyApp());
@@ -8,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Participants Lottery',
+      debugShowCheckedModeBanner: false,
+      title: 'Osallistujien arvonta',
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
-      home: MyHomePage(title: 'Participants Lottery'),
+      home: MyHomePage(title: 'Osallistujien arvonta'),
     );
   }
 }
@@ -27,12 +30,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  CountDownController _countDownController = CountDownController();
+  int _duration = 90;
+  TextEditingController _textEditingController = TextEditingController();
+  String codeDialog = '';
+  String valueText = '';
 
-  void _incrementCounter() {
+  void _runLottery(){
     setState(() {
-      _counter++;
+      codeDialog = lottery.runLottery(codeDialog);
     });
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Syötä nimet'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textEditingController,
+              decoration: InputDecoration(hintText: "Syötä nimet, erottele pilkuilla"),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    codeDialog = valueText;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+
+            ],
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 
   @override
@@ -40,23 +92,104 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: (){
+              _displayTextInputDialog(context);
+            },
+            icon: const Icon(Icons.settings),
+            tooltip: 'Asetukset',
+          )
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Järjestys',
+              'Puheaika',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            CircularCountDownTimer(
+                width: MediaQuery.of(context).size.width/2,
+                height: MediaQuery.of(context).size.height/2,
+                duration: _duration,
+                initialDuration: 0,
+                fillColor: Colors.greenAccent[700]!,
+                ringColor: Colors.grey[300]!,
+                controller: _countDownController,
+                ringGradient: null,
+                fillGradient: null,
+                backgroundColor: Colors.green[700],
+                backgroundGradient: null,
+                strokeWidth: 20.0,
+                strokeCap: StrokeCap.round,
+                textStyle: TextStyle(
+                  fontSize: 33.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+                textFormat: CountdownTextFormat.S,
+                isReverse: true,
+                isReverseAnimation: true,
+                isTimerTextShown: true,
+                autoStart: false,
+                onStart: () {
+                  print('Countdown Started');
+                },
+                onComplete: () {
+                  print('Countdown Ended');
+                },
+            ),
+            Text(
+              'Järjestys:',
               style: Theme.of(context).textTheme.headline4,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '$codeDialog',
+              style: Theme.of(context).textTheme.headline6,
             ),
-            ElevatedButton.icon(onPressed: _incrementCounter, icon: const Icon(Icons.shuffle,size: 18,), label: Text("Arvo"))
+            ElevatedButton.icon(onPressed: (){_runLottery();}, icon: const Icon(Icons.shuffle,size: 18,), label: Text("Arvo"))
           ],
         ),
       ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 30,
+          ),
+          _button(title: "Start", onPressed: () => _countDownController.start()),
+          SizedBox(
+            width: 10,
+          ),
+          _button(title: "Pause", onPressed: () => _countDownController.pause()),
+          SizedBox(
+            width: 10,
+          ),
+          _button(title: "Resume", onPressed: () => _countDownController.resume()),
+          SizedBox(
+            width: 10,
+          ),
+          _button(
+              title: "Restart",
+              onPressed: () => _countDownController.restart(duration: _duration))
+        ],
+      ),
     );
+  }
+
+  _button({required String title, VoidCallback? onPressed}) {
+    return Expanded(
+        child: ElevatedButton(
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              onPrimary: Colors.white,
+            )
+        ));
   }
 }
