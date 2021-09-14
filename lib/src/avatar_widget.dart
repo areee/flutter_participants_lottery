f9560d4e51c721a5c674f23ea7284dddbcd7cf9f
areee <10089872+areee@users.noreply.github.com>
2021-09-14 22:41:18 +0300

    Add avatar_widget.dart (use Multiavatar API key from .env file)

---
+++ lib/src/avatar_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AvatarWidget extends StatelessWidget {
+  final String participantName;
+
+  const AvatarWidget({Key? key, required this.participantName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(
+            'https://api.multiavatar.com/$participantName.png?apikey=${dotenv.env['MULTIAVATAR_API_KEY']}',
+            height: 50,
+          ),
+          Text('$participantName')
+        ],
+      ),
+    );
+  }
+}
