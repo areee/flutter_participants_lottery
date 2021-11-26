ad572c34a964f91bffd5227bd65926b64ce5df96
areee <10089872+areee@users.noreply.github.com>
2021-11-26 12:55:37 +0200

    Add 'fpl_theme.dart' & google_fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'fpl_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,20 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = ThemeData(
-        brightness: Brightness.light, primaryColor: Colors.lightGreen);
-
-    final ThemeData darkTheme =
-        ThemeData(brightness: Brightness.dark, primaryColor: Colors.lightGreen);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Osallistujien arvonta',
-      theme: theme.copyWith(
-          colorScheme: theme.colorScheme.copyWith(secondary: Colors.white)),
-      darkTheme: darkTheme.copyWith(
-          colorScheme: darkTheme.colorScheme.copyWith(secondary: Colors.black)),
-      home: HomePage(title: 'Osallistujien arvonta'),
+      theme: FplTheme.light(),
+      darkTheme: FplTheme.dark(),
+      home: const HomePage(title: 'Osallistujien arvonta'),
     );
   }
 }
