5be0b09986491a026e543aebe4973760cb2ad6e6
areee <10089872+areee@users.noreply.github.com>
2022-01-16 20:19:12 +0200

    Update fpl_theme (new Google Font & new colors)

--- lib/fpl_theme.dart
+++ lib/fpl_theme.dart
@@ -3,32 +3,32 @@ import 'package:google_fonts/google_fonts.dart';
 
 class FplTheme {
   static TextTheme lightTextTheme = TextTheme(
-    bodyText1: GoogleFonts.pacifico(
+    bodyText1: GoogleFonts.cevicheOne(
         fontSize: 14.0, fontWeight: FontWeight.w700, color: Colors.black),
-    subtitle1: GoogleFonts.pacifico(
+    subtitle1: GoogleFonts.cevicheOne(
         fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black),
-    subtitle2: GoogleFonts.pacifico(
+    subtitle2: GoogleFonts.cevicheOne(
         fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
-    headline1: GoogleFonts.pacifico(
+    headline1: GoogleFonts.cevicheOne(
         fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
-    headline2: GoogleFonts.pacifico(
+    headline2: GoogleFonts.cevicheOne(
         fontSize: 21.0, fontWeight: FontWeight.w700, color: Colors.black),
-    headline3: GoogleFonts.pacifico(
+    headline3: GoogleFonts.cevicheOne(
         fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.black),
   );
 
   static TextTheme darkTextTheme = TextTheme(
-    bodyText1: GoogleFonts.pacifico(
+    bodyText1: GoogleFonts.cevicheOne(
         fontSize: 14.0, fontWeight: FontWeight.w600, color: Colors.white),
-    subtitle1: GoogleFonts.pacifico(
+    subtitle1: GoogleFonts.cevicheOne(
         fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white),
-    subtitle2: GoogleFonts.pacifico(
+    subtitle2: GoogleFonts.cevicheOne(
         fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
-    headline1: GoogleFonts.pacifico(
+    headline1: GoogleFonts.cevicheOne(
         fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
-    headline2: GoogleFonts.pacifico(
+    headline2: GoogleFonts.cevicheOne(
         fontSize: 21.0, fontWeight: FontWeight.w700, color: Colors.white),
-    headline3: GoogleFonts.pacifico(
+    headline3: GoogleFonts.cevicheOne(
         fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
   );
 
@@ -36,12 +36,12 @@ class FplTheme {
     return ThemeData(
       brightness: Brightness.light,
       colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: Colors.red,
-        secondary: Colors.redAccent,
+        primary: Color.fromRGBO(2, 48, 89, 1),
+        secondary: Color.fromRGBO(60, 116, 166, 1),
         brightness: Brightness.light,
       ),
       textSelectionTheme: const TextSelectionThemeData(
-        selectionColor: Colors.orange,
+        selectionColor: Color.fromRGBO(2, 56, 89, 1),
       ),
       textTheme: lightTextTheme,
     );
@@ -50,13 +50,13 @@ class FplTheme {
   static dark() {
     return ThemeData(
       brightness: Brightness.dark,
-      primaryColor: Colors.red,
+      primaryColor: Color.fromRGBO(2, 48, 89, 1),
       appBarTheme: AppBarTheme(
-        backgroundColor: Colors.red[300],
+        backgroundColor: Color.fromRGBO(138, 161, 191, 1),
       ),
       colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: Colors.red,
-        secondary: Colors.redAccent,
+        primary: Color.fromRGBO(2, 48, 89, 1),
+        secondary: Color.fromRGBO(60, 116, 166, 1),
         brightness: Brightness.dark,
       ),
       textTheme: darkTextTheme,
