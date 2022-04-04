f4d5d9bfa1dd661e3a82d787fff64c232f68faf3
areee <10089872+areee@users.noreply.github.com>
2022-04-04 21:01:40 +0300

    Update custom_app_bar.dart (add background color for the text)

--- lib/components/custom_app_bar.dart
+++ lib/components/custom_app_bar.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 PreferredSizeWidget customAppBar(
-    BuildContext context, Widget title, List<Widget>? actions) {
+    BuildContext context, String title, List<Widget>? actions) {
   return AppBar(
     backgroundColor: Theme.of(context).colorScheme.primary,
     foregroundColor: Colors.black,
-    title: title,
+    title: Text(
+      title,
+      style: TextStyle(
+        background: Paint()
+          ..color = Colors.green.withOpacity(0.5)
+          ..strokeWidth = 17
+          ..strokeJoin = StrokeJoin.round
+          ..strokeCap = StrokeCap.round
+          ..style = PaintingStyle.stroke,
+      ),
+    ),
     centerTitle: true,
     flexibleSpace: Container(
       decoration: const BoxDecoration(
