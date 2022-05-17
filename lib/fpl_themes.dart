a4b8fd7e9f79b3ed1dd468b6c29e69d026ecf578
areee <10089872+areee@users.noreply.github.com>
2022-05-17 21:23:43 +0300

    Move themes to own private sub-methods (for the future)

--- lib/fpl_themes.dart
+++ lib/fpl_themes.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightFplTheme(BuildContext context) {
+  return _lightSpring2022Theme(context);
+}
+
+ThemeData darkFplTheme(BuildContext context) {
+  return _darkSpring2022Theme(context);
+}
+
+ThemeData _lightSpring2022Theme(BuildContext context) {
   return ThemeData(
     colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.blue, brightness: Brightness.light)
+            primarySwatch: Colors.green, brightness: Brightness.light)
         .copyWith(secondary: Colors.yellowAccent[700]),
     textTheme: GoogleFonts.fredokaOneTextTheme(
       Theme.of(context).textTheme,
@@ -12,16 +20,16 @@ ThemeData lightFplTheme(BuildContext context) {
   );
 }
 
-ThemeData darkFplTheme(BuildContext context) {
+ThemeData _darkSpring2022Theme(BuildContext context) {
   return ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.green, brightness: Brightness.dark)
+        .copyWith(secondary: Colors.yellowAccent[700]),
     textTheme: GoogleFonts.fredokaOneTextTheme(
       Theme.of(context).textTheme.apply(
             bodyColor: Colors.white,
             displayColor: Colors.white,
           ),
     ),
-    colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.green, brightness: Brightness.dark)
-        .copyWith(secondary: Colors.yellowAccent[700]),
   );
 }
