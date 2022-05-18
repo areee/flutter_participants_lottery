076d409faab2435b602d3143a77279d141b5cc2d
areee <10089872+areee@users.noreply.github.com>
2022-05-18 21:02:07 +0300

    Remove asset image from the custom app bar

--- lib/components/custom_app_bar.dart
+++ lib/components/custom_app_bar.dart
@@ -4,27 +4,10 @@ PreferredSizeWidget customAppBar(
     BuildContext context, String title, List<Widget>? actions) {
   return AppBar(
     backgroundColor: Theme.of(context).colorScheme.primary,
-    foregroundColor: Colors.black,
     title: Text(
       title,
-      style: TextStyle(
-        background: Paint()
-          ..color = Theme.of(context).colorScheme.primary.withOpacity(0.5)
-          ..strokeWidth = 17
-          ..strokeJoin = StrokeJoin.round
-          ..strokeCap = StrokeCap.round
-          ..style = PaintingStyle.stroke,
-      ),
     ),
     centerTitle: true,
-    flexibleSpace: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          repeat: ImageRepeat.repeatX,
-          image: AssetImage('assets/images/kukkia_ruohikolla.png'),
-        ),
-      ),
-    ),
     actions: actions,
   );
 }
