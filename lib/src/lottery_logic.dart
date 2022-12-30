c0b1252a285bfae90b1a9f02d7d72dcd2675f229
areee <10089872+areee@users.noreply.github.com>
2022-12-30 20:55:43 +0200

    Fix runLotteryList method if no names

--- lib/src/lottery_logic.dart
+++ lib/src/lottery_logic.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_participants_lottery/extensions/string_extension.dart';
 
 List<String> runLotteryList(List<String> names) {
+  if (names.isEmpty) {
+    return names;
+  }
   names = names.map((e) => e.trimAndReplaceDoubleSpaces()).toList();
   // Shuffle (randomize) the names
   names.shuffle();
