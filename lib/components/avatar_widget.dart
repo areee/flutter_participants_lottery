d3411e381b826e40c7894cbbc990cc4e6bf546b7
areee <10089872+areee@users.noreply.github.com>
2023-01-15 21:47:11 +0200

    Rewrite AvatarWidget

---
+++ lib/components/avatar_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:random_avatar/random_avatar.dart';
+
+class AvatarWidget extends StatelessWidget {
+  final String name;
+
+  const AvatarWidget({Key? key, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        randomAvatar(name, width: 50, height: 50),
+        Text(
+          name,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ],
+    );
+  }
+}
